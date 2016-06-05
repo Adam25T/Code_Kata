b70b03ee716834eccdd15b7c12873fde3ee73ec4
Adam25T <ajturner@owu.edu>
2016-06-04 20:54:10 -0400

    4th Test (FAILED)
    get method for bedTime does not exist.
    Next Step: Create get method for 2nd parameter bedTime.

--- Code_KataTest.java
+++ Code_KataTest.java
@@ -9,6 +9,12 @@ public class Code_KataTest {
 		Hours hours = new Hours(1, 0, 0);
 		assertEquals("1", hours.getStartTime().toString());
 	}
+	
+	@Test
+	public void WhenHoursIsGivenBedTimeTwoItReturnsTwo(){
+		Hours hours = new Hours(1, 2, 0);
+		assertEqauls("2", hours.getBedTime().toString());
+	}
 
 	
 	
